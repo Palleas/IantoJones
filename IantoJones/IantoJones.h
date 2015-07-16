437b114dd63324f49992aa13c5ed5db94df84214
Romain Pouclet <romain.pouclet@gmail.com>
2015-07-16 16:01:25 -0400

    Initial commit with iOS framework

---
+++ IantoJones/IantoJones.h
@@ -0,0 +1,19 @@
+//
+//  IantoJones.h
+//  IantoJones
+//
+//  Created by Romain Pouclet on 2015-07-16.
+//  Copyright (c) 2015 Perfectly-Cooked. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IantoJones.
+FOUNDATION_EXPORT double IantoJonesVersionNumber;
+
+//! Project version string for IantoJones.
+FOUNDATION_EXPORT const unsigned char IantoJonesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IantoJones/PublicHeader.h>
+
+
